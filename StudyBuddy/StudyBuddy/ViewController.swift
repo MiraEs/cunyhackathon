772246834499cf619359fff02d162c5a60dfd9e9
Mira Estil <imz.estil@gmail.com>
2016-10-14 19:52:02 -0400

    init iOS project

---
+++ StudyBuddy/StudyBuddy/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  StudyBuddy
+//
+//  Created by Ilmira Estil on 10/14/16.
+//  Copyright © 2016 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+
+}
+
